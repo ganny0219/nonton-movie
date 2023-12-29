d4d8f2df05e097125bec5dda00e4c1388454684c
GANNY <ganny0219@gmail.com>
2023-12-29 08:50:57 +0700

    perbaikan meta robots

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
         <meta name="author" content="Moovie21" />
         <meta
           name="robots"
-          content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1"
+          content="max-image-preview:large, max-snippet:-1, max-video-preview:-1"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Quicksand&display=swap"
